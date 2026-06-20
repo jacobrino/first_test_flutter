8858c416302bead452d08faa329c3725b2d6133a
jacob_rino <jacobrino05@gmail.com>
2026-06-21 02:56:32 +0300

    Create another app_cycle_de_vie.dart to show on the first time, a page welcome and active the deactivate, dispose event

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:first_test/app.dart';
+//import 'package:first_test/app.dart';
+import 'package:first_test/app_cycle_de_vie.dart';
+
 void main() {
   runApp(const FirstActivity());
 }
